8c769ecfe215d4403d05834f5748458f7e8b14f0
Gleb Kislovskyi <gkislovskyi@gmail.com>
2026-05-28 11:52:44 +0300

    Update Task2.py with lottery ticket generator function

--- Task2.py
+++ Task2.py
@@ -1,1 +1,21 @@
+import random
 
+def get_numbers_ticket(min, max, quantity):
+    if min < 1:
+        return []
+    if max > 1000:
+        return []
+    if quantity < 1 or quantity > (max - min + 1):
+        return []
+    result = set()    
+    while len(result) < quantity:
+        number = random.randint(min, max)
+        result.add(number)
+    return sorted(result)
+
+res = get_numbers_ticket(2, 1001, 5)
+print(res)
+res = get_numbers_ticket(10, 100, 9)
+print(res)
+res = get_numbers_ticket(3, 100, 6)
+print(res)
